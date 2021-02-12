e290676adf1f9eed01a9e39e440487d5c59ab6b3
Martin Cabrera <martin@kisland.com>
2021-02-12 10:56:01 +0100

    adds erlport

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule PythonElixir.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:erlport, "~>0.10.1"}
     ]
   end
 end
