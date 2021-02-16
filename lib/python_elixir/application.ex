be1298188b23f295e383f030ed7f4a8c05e93fe9
Martin Cabrera <martin@kisland.com>
2021-02-16 23:47:37 +0100

    adds poolboy configuration

--- lib/python_elixir/application.ex
+++ lib/python_elixir/application.ex
@@ -9,11 +9,21 @@ defmodule PythonElixir.Application do
     children = [
       # Starts a worker by calling: PythonElixir.Worker.start_link(arg)
       # {PythonElixir.Worker, arg}
+      :poolboy.child_spec(:worker, python_poolboy_config())
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: PythonElixir.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp python_poolboy_config do
+    [
+      {:name, {:local, :python_worker}},
+      {:worker_module, PythonElixir.PythonWorker},
+      {:size, 5},
+      {:max_overflow, 0}
+    ]
   end
 end
